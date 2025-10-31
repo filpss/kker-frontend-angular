7a0bef41513fe6a8930bcc5b238859e904afcee6
Rodrigo Santos <rodrigosantossilva1557@gmail.com>
2025-10-31 19:35:34 -0300

    feat: escopo formulário cliente

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,16 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'outline',
+                  floatLabel: 'always'
+      }
+    },
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes)
