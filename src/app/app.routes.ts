f30701d2e25326be194e74120c005d480247c1f5
Rodrigo Santos <rodrigosantossilva1557@gmail.com>
2025-11-01 14:18:53 -0300

    fix: correção nas pastas de cliente component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { ClienteForm } from './components/cliente-form/cliente-form';
+import { ClienteForm } from './pages/cliente/cliente-form';
 import { Home } from './pages/home/home';
 
 export const routes: Routes = [
