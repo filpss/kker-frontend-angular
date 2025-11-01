8ab8978b4d2c117a87bc4d5ec5bf34f6d3085bd5
Filipe Pinho <filipepinho2010@hotmail.com>
2025-11-01 01:45:58 -0300

    wip

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { ClienteForm } from './components/cliente/cliente-form/cliente-form';
+import { ClienteForm } from './components/cliente-form/cliente-form';
 
 export const routes: Routes = [
 
-  {path: 'cliente', component: ClienteForm}
+  {path: 'clientes', component: ClienteForm}
 ];
