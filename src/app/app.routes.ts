4cefb35dacc982dfa2e3971ea330b1cbcb147a1e
Rodrigo Santos <rodrigosantossilva1557@gmail.com>
2025-11-11 16:11:10 -0300

    feat: criação das rotas de venda

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { ClienteForm } from './pages/cliente/form/cliente-form';
 import { Home } from './pages/home/home';
+import { VendaForm } from './pages/venda/form/venda-form/venda-form';
 
 export const routes: Routes = [
   {path: '', component: Home},
   {path: 'clientes', component: ClienteForm},
-  {path: 'clientes/:id', component: ClienteForm}
+  {path: 'clientes/:id', component: ClienteForm},
+
+  {path: 'venda', component: VendaForm},
+  {path: 'venda/:id', component: VendaForm}
 ];
