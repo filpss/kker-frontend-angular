5c124e00ba9d7586ffa39c70b7a533d33cd530e1
Rodrigo Santos <rodrigosantossilva1557@gmail.com>
2025-11-14 22:52:04 -0300

    feat: model de cliente

---
+++ src/app/model/cliente-entity.ts
@@ -0,0 +1,9 @@
+import { StatusCliente } from "../shared/enums/StatusCliente";
+
+export interface Cliente {
+  id: number;
+  nome: string;
+  telefone: string;
+  isAtivo: StatusCliente;
+  dataCobranca: Date | string;
+}
